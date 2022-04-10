9f3afa038712f94dad20cf382b5c9ddb880af8cb
Yury Feschenko <ronerycamel@gmail.com>
2022-04-10 18:32:41 +0300

    Added Profile section

---
+++ components/SectionProfile/index.js
@@ -0,0 +1,23 @@
+export default function SectionProfile() {
+  return (
+    <section className="section section-secondary">
+      <div className="container">
+        <h2>About me</h2>
+        <p>
+          Frontend developer with&nbsp;4+ years of&nbsp;experience
+          and&nbsp;background in&nbsp;print layout design.
+        </p>
+        <p>
+          Worked mostly with&nbsp;Vue&nbsp;/ Nuxt.JS, but&nbsp;always open
+          to&nbsp;learn new things.
+        </p>
+        <p>
+          I have great attention to&nbsp;detail, always strive to&nbsp;do my job
+          the&nbsp;best way possible and&nbsp;think that good communication
+          is&nbsp;the&nbsp;key to&nbsp;success.
+        </p>
+        <p>My native language is Russian, but I speak English too.</p>
+      </div>
+    </section>
+  );
+}
