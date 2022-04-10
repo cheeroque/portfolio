358b40526da3a67d0687c2137607bcde21e39e4a
Yury Feschenko <ronerycamel@gmail.com>
2022-04-10 18:18:52 +0300

    Finished Hero section

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,18 +1,23 @@
-import Image from 'next/image';
-import heroImage from '../../public/images/me.png';
 import styles from './Hero.module.scss';
 
 export default function Hero() {
   return (
-    <section className={styles.section}>
+    <section className={styles.sectionHero}>
       <div className={styles.container + ' container'}>
-        <div className="row">
-          <div className="col-12 col-lg-7">
-            <h1>Hi, my name is&nbsp;Yuriy</h1>
-            <h2>and I am a&nbsp;frontend web&nbsp;developer</h2>
+        <div className={styles.row + ' row'}>
+          <div className="col-12 col-sm-8 col-md-7">
+            <h1 className="mb-8 mb-md-0">Hi, my name is&nbsp;Yuriy</h1>
+            <h2 className="mb-0">
+              and I am a&nbsp;frontend web&nbsp;developer
+            </h2>
           </div>
-          <div className="col-12 col-lg-5">
-            <Image src={heroImage} className={styles.heroImage} />
+          <div className="col-12 col-sm-4 col-md-5">
+            <img
+              srcset="/images/me-336w.webp 336w, /images/me-420w.webp 420w, /images/me-620w.webp 620w"
+              sizes="(max-width: 767px) 336px, (max-width: 991px) 420px, 620px"
+              src="/images/me-620w.png"
+              className={styles.heroImage}
+            />
           </div>
         </div>
       </div>
